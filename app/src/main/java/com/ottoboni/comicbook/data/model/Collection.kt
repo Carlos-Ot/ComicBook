25981ff6013af0a59bae52ffe517f3d51fb1ed39
Carlos Ottoboni <caoj@cesar.org.br>
2018-03-13 13:48:44 -0300

    Resolving publishing name for collections

--- app/src/main/java/com/ottoboni/comicbook/data/model/Collection.kt
+++ app/src/main/java/com/ottoboni/comicbook/data/model/Collection.kt
@@ -3,7 +3,7 @@ package com.ottoboni.comicbook.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Collection constructor(
-        @SerializedName("collecion_id") var collectionId: String,
+        @SerializedName("collection_id") var collectionId: String,
         @SerializedName("collection_name") var collectionName:String,
         @SerializedName("collection_url") var collectionUrl: String,
         @SerializedName("status") var status: Int,
--- app/src/main/java/com/ottoboni/comicbook/data/repositories/MainRepository.kt
+++ app/src/main/java/com/ottoboni/comicbook/data/repositories/MainRepository.kt
@@ -4,25 +4,41 @@ import android.util.Log
 import com.ottoboni.comicbook.data.model.Collection
 import com.ottoboni.comicbook.data.model.Publishing
 import com.ottoboni.comicbook.data.source.remote.ServiceClient
+import io.reactivex.Observable
+import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by caoj on 03/03/18.
  */
 
-class MainRepository{
+class MainRepository {
 
     private val apiClient = ServiceClient().getApiClient()
 
     fun getCollections(callback: RequestCallback) {
         apiClient.getCollections()
                 .subscribeOn(Schedulers.io())
+                .flatMap { collections -> Observable.fromIterable(collections) }
+                .flatMap { collection: Collection ->
+                    Observable.zip(
+                            Observable.just(collection),
+                            apiClient.getPublishing(collection.publishing).toObservable(),
+                            BiFunction<Collection, Publishing, Collection> { collectionReturn, publishingReturn ->
+                                collectionReturn.apply {
+                                    this.publishingName = publishingReturn.publishingName
+                                }
+                            }
+                    )
+                }.toList().toObservable()
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    collections: List<Collection> -> callback.onSuccess(collections)
-                },{
-                    error -> Log.e("xablau", error.message)
+                .subscribe({ collections: List<Collection> ->
+                    callback.onSuccess(collections)
+                }, { error ->
+                    Log.e("xablau", error.message)
                     callback.onError()
                 })
     }
